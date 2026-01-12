4c9b39dad21cd4a673626ab0c8ba0139bf1f55c2
Jordi Armentia <jordiarmentia@gmail.com>
2026-01-12 18:13:28 +0000

    Updating Dockerfile

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -30,25 +31,22 @@ parse_dates = [
 ]
 
 
-def main():
-
-    year = "2021"
-    month = "01"
+@click.command()
+@click.option("--year", default=2021, type=int, help="Year of the data")
+@click.option("--month", default=1, type=int, help="Month of the data")
+@click.option("--pg-user", default="root", help="PostgreSQL user")
+@click.option("--pg-pass", default="root", help="PostgreSQL password")
+@click.option("--pg-host", default="localhost", help="PostgreSQL host")
+@click.option("--pg-port", default="5432", help="PostgreSQL port")
+@click.option("--pg-db", default="ny_taxi", help="PostgreSQL database name")
+@click.option("--target-table", default="yellow_taxi_data", help="Target table name")
+@click.option("--chunksize", default=100000, type=int, help="Chunk size for data processing")
+def main(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize):
 
     url_prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
-    url = f"{url_prefix}yellow_tripdata_{year}-{month}.csv.gz"
-
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = "5432"
-    pg_db = "ny_taxi"
-
-    chunksize = 100000
+    url = f"{url_prefix}yellow_tripdata_{year:04d}-{month:02d}.csv.gz"
 
     first = True
-
-    df = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates)
 
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
     df_iter = pd.read_csv(
@@ -61,9 +59,9 @@ def main():
 
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(0).to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
             first = False
-        df_chunk.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
 
 
 
