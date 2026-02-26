689934c440703c6e66245ca74c9f64e23d9c31fa
Jordi Armentia <jordiarmentia@gmail.com>
2026-02-26 22:04:49 +0100

    06-batch-processing

---
+++ 06-batch-processing/test_spark.py
@@ -0,0 +1,16 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+print(f"Spark version: {spark.version}")
+
+df = spark.range(10)
+df.show()
+
+input("press enter to stop Spark session")
+
+spark.stop()
